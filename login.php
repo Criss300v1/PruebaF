458a40789afefe54340fb467a7517be32a36300a
Cristian <cristianelpro004@gmail.com>
2023-05-01 16:38:00 -0500

    se añadio un login basico a login.php

---
+++ login.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Iniciar sesión</title>
+</head>
+<body>
+	<h2>Iniciar sesión</h2>
+	<form method="POST" action="login_process.php">
+		<label>Nombre de usuario:</label>
+		<input type="text" name="username"><br>
+		<label>Contraseña:</label>
+		<input type="password" name="password"><br>
+		<input type="submit" value="Iniciar sesión">
+	</form>
+</body>
+</html>
